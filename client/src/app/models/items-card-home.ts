4cdd7f01dff94e2b1b1973a1da65b28b7d38f970
Agatha Sates <satesagatha@gmail.com>
2025-11-09 02:08:05 -0300

    feat Adicionado dados de ItemsCardHome

---
+++ client/src/app/models/items-card-home.ts
@@ -0,0 +1,14 @@
+export interface ItemsCardHome {
+  moduleName: string;
+  ariaLabelButton: string;
+  button1Title: string;
+  button1Link: string;
+  imageSrc: string;
+  imageAlt: string;
+  title: string;
+  description: string;
+  buttonIcon: string;
+  button2Title: string;
+  buttonText: string;
+  button2Link: string;
+}
